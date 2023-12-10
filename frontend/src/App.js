eb8c934af46d4463c2ff19f8fb1005da1520f3e9
liannnaa <lnp2016@nyu.edu>
2023-12-10 01:47:59 -0500

    added swipe buttons

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './Header';
 import TinderCards from './TinderCards';
+import SwipeButtons from './SwipeButtons';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 
@@ -10,7 +11,15 @@ function App() {
       <Header />
       <Router>
         <Routes>
-          <Route path="/" element={<TinderCards/>}/>
+        <Route 
+            path="/" 
+            element={
+              <>
+                <TinderCards/>
+                <SwipeButtons/>
+              </>
+            }
+          />
         </Routes>
       </Router>
     </div>
