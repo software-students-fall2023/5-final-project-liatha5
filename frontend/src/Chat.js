8b044101032bf0b1152b9b9440195997fdd784ba
liannnaa <lnp2016@nyu.edu>
2023-12-10 18:45:41 -0500

    added chats

--- frontend/src/Chat.js
+++ frontend/src/Chat.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import './Chat.css';
 import Avatar from '@mui/material/Avatar';
+import { Link } from 'react-router-dom';
 
 function Chat({ name, message, profilePic, timestamp }) {
     return (
-        <div className='chat'>
-            <Avatar className='chat_image' src={profilePic}/>
-            <div className='chat_details'>
-                <h2>{name}</h2>
-                <p>{message}</p>
+        <Link to={`/chat/${name}`}>
+            <div className='chat'>
+                <Avatar className='chat_image' alt={name} src={profilePic}/>
+                <div className='chat_details'>
+                    <h2>{name}</h2>
+                    <p>{message}</p>
+                </div>
+                <p className='chat_timestamp'>{timestamp}</p>
             </div>
-            <p className='chat_timestamp'>{timestamp}</p>
-        </div>
+        </Link>
     );
 }
 
