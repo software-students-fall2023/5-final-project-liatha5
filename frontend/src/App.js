05709177f7e789c14d5f2478919daaa21b056e26
liannnaa <lnp2016@nyu.edu>
2023-12-10 15:27:39 -0500

    added chats page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,21 +2,41 @@ import React from 'react';
 import Header from './Header';
 import TinderCards from './TinderCards';
 import SwipeButtons from './SwipeButtons';
+import Chats from './Chats';
+import ChatScreen from './ChatScreen';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Header />
       <Router>
         <Routes>
-        <Route 
+          <Route 
             path="/" 
             element={
               <>
+                <Header/>
                 <TinderCards/>
                 <SwipeButtons/>
+              </>
+            }
+          />
+          <Route 
+            path="/chat" 
+            element={
+              <>
+                <Header backButton='/'/>
+                <Chats/>
+              </>
+            }
+          />
+          <Route 
+            path="/chat/:person" 
+            element={
+              <>
+                <Header backButton='/chat'/>
+                <ChatScreen />
               </>
             }
           />
