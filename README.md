aa5f42675052131c5c684a7a9bbf8cec929f9e01
aleong2002 <aleong2002@gmail.com>
2023-12-13 14:32:32 -0500

    backend setup

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # Final Project
 
-An exercise to put to practice software development teamwork, subsystem communication, containers, deployment, and CI/CD pipelines. See [instructions](./instructions.md) for details.
+## Project Description
+a plain-language description of your project, including:
+- badges at the top of the README.md file showing the result of the latest CI/CD of each subsystem.
+- links to the container images for each custom subsystem, hosted on DockerHub.
+
+## Project Members
+- [Athena Leong](https://github.com/aleong2002)
+- [Harry Minsky](https://github.com/hminsky2002)
+- [Lianna Poblete](https://github.com/liannnaa)
+
+## Configuration Instructions
+- instructions for how to configure and run all parts of your project for any developer on any platform - these instructions must work!
+- instructions for how to set up any environment variables and import any starter data into the database, as necessary, for the system to operate correctly when run.
---
+++ backend/app.js
@@ -0,0 +1,17 @@
+require('dotenv').config({ silent: true }) // load environmental variables from a hidden file named .env
+const express = require('express') // CommonJS import style!
+const morgan = require('morgan') // middleware for nice logging of incoming HTTP requests
+const cors = require('cors') // middleware for enabling CORS (Cross-Origin Resource Sharing) requests.
+const mongoose = require('mongoose')
+
+const app = express() // instantiate an Express object
+app.use(morgan('dev', { skip: (req, res) => process.env.NODE_ENV === 'test' })) // log all incoming requests, except when in unit test mode.  morgan has a few logging default styles - dev is a nice concise color-coded style
+app.use(cors()) // allow cross-origin resource sharing
+
+// use express's builtin body-parser middleware to parse any data included in a request
+app.use(express.json()) // decode JSON-formatted incoming POST data
+app.use(express.urlencoded({ extended: true })) // decode url-encoded incoming POST data
+
+
+// export the express app we created to make it available to other modules
+module.exports = app // CommonJS export style!
